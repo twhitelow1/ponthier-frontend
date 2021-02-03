a3bf2fc33ab17acc5d45c63da22ad64c72dfd2fa
Todd Whitelow <todd@whitelow.tech>
2021-02-02 21:40:16 -0700

    add jumbotron component

---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="jumbotron jumbotron-fluid" style="background: #18597d url('img/woodwash-filtered.jpg') no-repeat
+      center; background-size: cover;">
+    <!-- Start Jumbotron-->
+        <div class="container text-white">
+            <h1 class="text-center" id="jumbotron-heading">Ocala's Top <br> Pressure Washing <br>Professionals</h1>
+        </div>
+        <p class="text-center">
+            <a class="btn btn-info btn-lg" href="quote.html" role="button">Get A Free Quote!</a>
+        </p>
+    <!-- End Jumbotron-->
+  </div>
+</template>
+<script>
+export default {};
+</script>
