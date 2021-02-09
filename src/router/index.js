f3c45befd5b0adcf878ee857ef1b31bb8584fe38
Todd Whitelow <todd@whitelow.tech>
2021-02-09 00:58:53 -0700

    add admin page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
 import Quote from '../views/Quote.vue';
+import Admin from '../views/Admin.vue';
 
 Vue.use(VueRouter);
 
@@ -15,6 +16,11 @@ const routes = [
     path: '/quote',
     name: 'Quote',
     component: Quote
+  },
+  {
+    path: '/acp',
+    name: 'Admin',
+    component: Admin
   },
   {
     path: '/about',
---
+++ src/views/Admin.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container-fluid py-3 section" id="admin">
+      <div class="row justify-content-center">
+        <h2 class="text-center py-4 display-2">Pressure Washing Company In Ocala, FL & Surrounding Areas</h2>
+      </div>
+      <div class="row align-items-center about">
+        <div class="col-lg px-8">
+          <p>Are you looking to enhance the look of your home? With Ponthier & Son Pressure Washing Company LLC, our pressure washing services are not only crucial to general home owning maintenance and longevity of your property, but will completely revamp the overall look over your home. Let us help you restore your home's exterior, roof, concrete and more to its original luster! Here’s a list of services we offer, but not limited to:
+          </p>
+          <ul>
+            <li>House washing</li>
+            <li>Roof cleaning</li>
+            <li>Concrete cleaning</li>
+            <li>Gutter cleaning</li>
+            <li>Rust removal</li>
+            <li>Oxidation restoration</li>
+            <li>Residential and Commercial properties</li>
+          </ul>
+          <p>
+            These services are guaranteed to boost the curb appeal and value of your home. Trust in our professionals to get the job done right. Don't just invest in your home; but in quality care! 
+          <p>
+          <p>
+            We cannot predict or change the weather, but we can protect your home from falling wayside to Florida's harsh climate. With humid temperatures, and constant storms, your property is at risk from mildew, mold and debris. Over time these harmful substances will cause decay and staining. Left unattended will result in expensive home repairs. But with Ponthier & Son, our year round cleaning services, will keep your property pristine and weather damage free! 
+          </p>
+          <p> 
+            Don't just take the word of professionals, hear from our satisfied customers why you will be in good hands! Ponthier & Son pressure Washing company is committed to getting the job done, especially when it comes to this service. You can count on us to be professional, timely, efficient, and satisfied every step of the way. 
+          </p>
+        </div>
+        <div class="col-lg-5">
+          <img id="family-photo" class="img-fluid rounded" src="/img/pressure-clean.webp">
+        </div>
+      </div>
+    </div>
+    <!--End about section-->
+</template>
+<script>
+export default {
+  components: {},
+  data: function () {
+    return {
+      photos: [],
+    };
+  },
+};
+</script>
