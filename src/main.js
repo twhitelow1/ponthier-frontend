dd0f86da6b142787ac8bb5190e7cea1693a6206c
Todd Whitelow <todd@whitelow.tech>
2021-03-19 00:00:59 -0600

    troubleshooting

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from "axios";
-import vuetify from './plugins/vuetify';
+import vuetify from '@/plugins/vuetify';
 
 axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
 
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  css: {
+    loaderOptions: {
+      sass: {
+        data: ``
+      }
+    }
+  }
 }
