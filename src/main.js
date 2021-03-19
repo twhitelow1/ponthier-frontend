00a7a7e1cddf9daaa79b85a97ce12c7bb2e355a0
Todd Whitelow <todd@whitelow.tech>
2021-03-19 00:16:33 -0600

    fix vuetify issue

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from "axios";
-import vuetify from '@/plugins/vuetify';
 
 axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
 
@@ -10,6 +9,5 @@ Vue.config.productionTip = false;
 
 new Vue({
   router,
-  vuetify,
   render: h => h(App)
 }).$mount('#app')
