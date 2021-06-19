a8506fcad8ed4908ea00a9865488f7c3b40f3266
Todd Whitelow <todd@whitelow.tech>
2021-06-19 07:32:55 -0600

    new jumbotron photo

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="jumbotron jumbotron-fluid" style="background: #18597d url('img/woodwash-filtered.jpg') no-repeat
+  <div class="jumbotron jumbotron-fluid" style="background: #18597d url('img/pstrailer.jpg') no-repeat
       center; background-size: cover;">
     <!-- Start Jumbotron-->
         <div class="container text-white">
-            <h1 class="text-center" id="jumbotron-heading">Ocala's Top <br> Pressure Washing <br>Professionals</h1>
+            <h1 class="text-center" id="jumbotron-heading">Ocala's Top Pressure Washing <br>Professionals</h1><br/><br /><br/>
         </div>
         <p class="text-center">
             <a class="btn btn-info btn-lg" href="/quote" role="button">Get A Free Quote!</a>
